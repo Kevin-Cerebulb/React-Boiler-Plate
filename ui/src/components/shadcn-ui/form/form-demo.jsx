5aca482d7c053b50fd9f2af9a4f1122a2b387e21
Kevin <kevinu774@gmail.com>
2025-01-06 22:42:22 +0530

    Form Demo[WIP]

---
+++ ui/src/components/shadcn-ui/form/form-demo.jsx
@@ -0,0 +1,99 @@
+import { FormSchema } from "@/lib/schemas";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./form";
+import { useForm } from "react-hook-form";
+import { Input } from "../input";
+import { Button } from "../button";
+import { MultiSelect } from "@/components/multi-select";
+import { options } from "@/lib/constant";
+
+export function FormDemo() {
+  const form = useForm({
+    resolver: yupResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      select: [],
+    },
+  });
+
+  const submitHandler = (data) => {
+    /* On Submit Code goes Here */
+    console.log("Form Submitted");
+    console.log(data);
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(submitHandler)}
+        className="w-2/3 space-y-6"
+      >
+        {/* Email Control */}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Enter your email"
+                  variant="secondary"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>{/* This is optional */}</FormDescription>
+              <FormMessage /> {/* This will show the error message */}
+            </FormItem>
+          )}
+        />
+
+        {/* Password Control */}
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your password" {...field} />
+              </FormControl>
+              <FormMessage /> {/* This will show the error message */}
+            </FormItem>
+          )}
+        />
+
+        {/* Select Control */}
+        <FormField
+          control={form.control}
+          name="select"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Select any Option</FormLabel>
+              <FormControl>
+                <MultiSelect
+                  getOptionValue={(option) => option?.value}
+                  onChange={(item) => field.onChange(item.value)}
+                  options={options}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage /> {/* This will show the error message */}
+            </FormItem>
+          )}
+        />
+
+        {/* Submit Button */}
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- ui/src/lib/schemas.js
+++ ui/src/lib/schemas.js
@@ -1,12 +1,41 @@
 import * as yup from "yup";
 
-export const LoginSchema = yup.object({
-  email: yup
-    .string()
-    .email("Enter a valid Email")
-    .required("Please enter your email"),
-  password: yup
-    .string()
-    .min(3, "Password must be at least 8 characters.")
-    .required("Please enter your Password"),
-}).required()
+/**
+ * Form Schema
+ */
+export const FormSchema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Enter a valid Email")
+      .required("Please enter your email"),
+    password: yup
+      .string()
+      .min(8, "Password must be at least 8 characters.")
+      .required("Please enter your Password"),
+    // confirmPassword: yup
+    //   .string()
+    //   .oneOf([yup.ref("password"), null], "Passwords must match")
+    //   .required("Please confirm your password"),
+    select: yup
+      .array()
+      .of(
+        yup.object({
+          label: yup.string().required(),
+          value: yup.string().required(),
+        })
+      ).required("Please select an option"),
+  }).required();
+
+export const LoginSchema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Enter a valid Email")
+      .required("Please enter your email"),
+    password: yup
+      .string()
+      .min(3, "Password must be at least 8 characters.")
+      .required("Please enter your Password"),
+  })
+  .required();
