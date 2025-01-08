22b524fac00396acdabbee9171d8df5628ac9920
Kevin-Cerebulb <kevin.usadadiya@cerebulb.com>
2025-01-08 17:13:44 +0530

    Login Form Decorating

--- ui/src/components/shadcn-ui/login/login-form.jsx
+++ ui/src/components/shadcn-ui/login/login-form.jsx
@@ -8,48 +8,108 @@ import {
   CardTitle,
 } from "@/components/shadcn-ui/card";
 import { Input } from "@/components/shadcn-ui/input";
-import { Label } from "@/components/shadcn-ui/label";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+import WordPullUp from "@/components/magic-ui/word-pull-up";
+import TypingAnimation from "@/components/magic-ui/typing-animation";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../form/form";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { LoginSchema } from "@/lib/schemas";
 
 export function LoginForm({ className, ...props }) {
+  const form = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const submitHandler = (data) => {
+    /* On Submit Code goes Here */
+    console.log("Form Submitted");
+    console.log(data);
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
+      <Card className="backdrop-blur-sm bg-card-glass border border-white/20 rounded-xl shadow-lg px-6 max-w-sm">
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back</CardTitle>
-          <CardDescription>Login with your credentials</CardDescription>
+          <CardTitle className="text-xl">
+            <TypingAnimation className="text-xl">Welcome back</TypingAnimation>
+          </CardTitle>
+          <CardDescription>
+            <WordPullUp className="text-sm">
+              Login with your credentials
+            </WordPullUp>
+          </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="grid gap-6">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(submitHandler)}>
               <div className="grid gap-6">
                 <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="user@example.com"
-                    required
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <Link
-                      to="/forgot-password"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </Link>
+                  <div className="grid gap-2">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Enter your email"
+                              variant="ghost"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />{" "}
+                        </FormItem>
+                      )}
+                    />
                   </div>
-                  <Input id="password" type="password" required />
+                  <div className="grid gap-2">
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Enter your password"
+                              variant="ghost"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />{" "}
+                          <FormDescription className="flex justify-end text-foreground">
+                            <Link
+                              to="/forgot-password"
+                              className="ml-auto text-sm underline-offset-4 hover:underline mt-3 mb-2"
+                            >
+                              Forgot your password?
+                            </Link>
+                          </FormDescription>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <Button type="submit" className="w-full">
+                    Login
+                  </Button>
                 </div>
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
               </div>
-            </div>
-          </form>
+            </form>
+          </Form>
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
